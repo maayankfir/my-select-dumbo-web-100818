12512f46aadc533e2f0dfb4bccd9f9c70930e027
Maayan Kfir <mayankfir@gmail.com>
2018-09-24 19:26:26 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- collection.select {|num| num.even? }
+ i = 0 
+ select = []
+ while i < collection.length 
+   if yield(collection[i])
+     select.push(collection[i])
+   end
+   i += 1
+ end
+ select
 end
